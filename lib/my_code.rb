fc8570574befb73893eec6615918e742e9f2722d
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 17:00:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,9 +10,17 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point=nil)
-  i = 0
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+
   while i < source_array.length
-      yield(source_array[i])
+      sum = yield(source_array[i])
       i += 1
   end
+  sum
 end
