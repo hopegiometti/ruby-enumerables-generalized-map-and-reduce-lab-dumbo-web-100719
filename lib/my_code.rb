af573f2f3cb17c12b72b09e27f3bf5cecb3c7dbf
Hope Giometti <hopegiometti@gmail.com>
2019-09-23 16:56:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point=0)
+def reduce(source_array, starting_point=nil)
   i = 0
   while i < source_array.length
       yield(source_array[i])
